32e8b84b447a667194773197d499d817b315d386
Keenah Vancampenhout <kvancamp@mail.com>
2021-02-14 18:25:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is __"
+    "My name is Keenah"
   end
+
+  get '/hometown' do 
+    "My hometown is Green Bay"
+  end
+  get '/favorite-song'
+    "My favorite song is Come Join The Murder"
 end
